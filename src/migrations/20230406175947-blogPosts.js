3a4a7e69f34b0a62acae016379ca452a1bd43439
eduardoSousa <dudus.sousa4002@gmail.com>
2023-04-06 15:23:47 -0300

    requisito 1 concluido

---
+++ src/migrations/20230406175947-blogPosts.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('blog_posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      published: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('blog_posts');
+  }
+};
