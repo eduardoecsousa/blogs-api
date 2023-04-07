f931942f05e501fdf4de24417a9cd561a630bbc1
eduardoSousa <dudus.sousa4002@gmail.com>
2023-04-07 18:26:55 -0300

    requisito 11 concluido

---
+++ src/models/PostCategory.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory',
+  {
+    postId: { type: DataTypes.INTEGER, primaryKey: true },
+    categoryId: { type: DataTypes.INTEGER, primaryKey: true },
+  },
+  {
+    timestamps: false,
+    underscored: true,
+  });
+
+  PostCategory.associate = ({ Category, BlogPost }) => {
+    Category.belongsToMany(BlogPost,
+      { 
+        foreignKey: 'category_id', 
+        otherKey: 'post_id',
+        as: 'blogs',
+        through: PostCategory,
+      },
+    );
+
+    BlogPost.belongsToMany(Category,
+      { 
+        foreignKey: 'post_id', 
+        otherKey: 'category_id',
+        as: 'categories',
+        through: PostCategory,
+      }
+    );
+  }
+
+  return PostCategory;
+};
